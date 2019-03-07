73782aa8ac7f380900606c3aa0a52ad778e47250
Ny4nc4t <>
2019-03-07 18:11:50 +0100

    qw

--- gadgets.py
+++ gadgets.py
@@ -28,7 +28,7 @@ def convertXCS(s):
 
 ##############################################################
 
-def getHexStreamsFromElfExecutableSections(filename, length):
+def getHexStreamsFromElfExecutableSections(filename):
     print "Processing file:", filename
     with open(filename, 'rb') as f:
         elffile = ELFFile(f)
@@ -60,11 +60,9 @@ def getHexStreamsFromElfExecutableSections(filename, length):
             newExecSection['name'] = name
             newExecSection['addr'] = addr
             newExecSection['hexStream'] = hexStream
-            if hexStream.length == length :
-                print hexStream
-                print "\n"
-                if hexStream[-2:] == 'c3':
-                    execSections.append(newExecSection)
+            print hexStream
+            print "\n"
+            execSections.append(newExecSection)
 
         return execSections
 
@@ -75,18 +73,30 @@ if __name__ == '__main__':
             md = Cs(CS_ARCH_X86, CS_MODE_64)
             for filename in sys.argv[4:]:
                 length = sys.argv[3]
-                r = getHexStreamsFromElfExecutableSections(filename, length)
+                r = getHexStreamsFromElfExecutableSections(filename)
                 print "Found ", len(r), " executable sections:"
                 i = 0
                 for s in r:
                     print "   ", i, ": ", s['name'], "0x", hex(s['addr']), s['hexStream']
                     i += 1
-	                
                     hexdata = s['hexStream']
-                    gadget = hexdata[0 : 10]
-                    gadget = convertXCS(gadget)
-                    offset = 0
-                    for (address, size, mnemonic, op_str) in md.disasm_lite(gadget, offset):
-                        print ("gadget: %s %s \n") %(mnemonic, op_str)
 
-            
+                    #Part to find ret instructions and extract gadget
+
+                    ret = 'c3'
+                    positions = ([pos for pos, str in enumerate(s) if str == ret])
+                    for pos in positions:
+                        gadget = hexdata[pos-4: pos+2]
+                        gadget = convertXCS(gadget)
+                        offset = 0
+                        for (address, size, mnemonic, op_str) in md.disasm_lite(gadget, offset):
+                            print ("gadget: %s %s \n") %(mnemonic, op_str)
+
+
+def index_ret(string, sub_string):
+    cnt = 0
+    len_ss = len(sub_string)
+    for i in range(len(string) - len_ss + 1):
+        if string[i:i+len_ss] == sub_string:
+            cnt += 1
+    return cnt
