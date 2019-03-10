df0dfe1eebdc037d25997a285c1c310b62e4d39e
Ny4nc4t <user@debian>
2019-03-10 05:33:14 -0400

    q

---
+++ out.d
@@ -0,0 +1,65 @@
+Processing file: /lib/x86_64-linux-gnu/libc-2.24.so
+Found  1  executable sections:
+    0 :  .text 0x 0x1f940
+gadget at 132bae : 
+
+e      syscall  
+
+10      ret  
+
+gadget at 132bce : 
+
+e      syscall  
+
+10      ret  
+
+gadget at 132bee : 
+
+e      syscall  
+
+10      ret  
+
+gadget at 132c0e : 
+
+e      syscall  
+
+10      ret  
+
+gadget at 132c2e : 
+
+e      syscall  
+
+10      ret  
+
+gadget at 132c4e : 
+
+e      syscall  
+
+10      ret  
+
+gadget at 13306e : 
+
+e      syscall  
+
+10      ret  
+
+gadget at 17788e : 
+
+e      syscall  
+
+10      ret  
+
+gadget at 192f4e : 
+
+e      syscall  
+
+10      ret  
+
+gadget at 1c6074 : 
+
+e      syscall  
+
+10      ret  
+
+10
+4261
