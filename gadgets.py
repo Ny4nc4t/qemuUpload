a20cc36c9aa9013accc5f7170bca01929b34f748
Ny4nc4t <>
2019-03-13 20:10:29 +0100

    qw

--- gadgets.py
+++ gadgets.py
@@ -93,7 +93,8 @@ if __name__ == '__main__':
                             # takes the bytes before ret, depending on the length specified
                             gadget = hexdata[i - lengthHex: i + 2]
                             print gadget
-                            #gadget = convertXCS(gadget)
+                            gadget = convertXCS(gadget)
+                            print gadget
                             offset = 0
                             # counts number of return functions discovered
                             nbret += 1
