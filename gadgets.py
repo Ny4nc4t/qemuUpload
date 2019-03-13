6ea3dfbf06902821a8f79b0c0af3e97ea2836a61
Ny4nc4t <>
2019-03-13 14:40:02 +0100

    qw

--- gadgets.py
+++ gadgets.py
@@ -86,35 +86,48 @@ if __name__ == '__main__':
                     #Part to find ret instructions and extract gadget
                     badInstruct = ['jmp', 'jmpq', 'jne', 'js', 'jns','jg', 'jge', 'je', 'callq', 'call', 'jb', 'jbe','leave', 'ret', 'retq', 'retf', 'retn']
                     ret = ['c3', 'cb', 'c2', 'ca']
-                    for i, _ in enumerate(hexdata):
-                        if str(hexdata[i:i + 2]) in ret:
+                    for i, _ in enumerate(hexdata):  # loops through hex string
+                        # TODO the problem might be here. Splitting arbitrarily hex string might result into wrong
+                        # assembly instructions and thus wrong gadgets
+                        if str(hexdata[i:i + 2]) in ret:  # if it finds a ret instruction in hex it gets in the if
+                            # takes the bytes before ret, depending on the length specified
                             gadget = hexdata[i - lengthHex: i + 2]
+
                             gadget = convertXCS(gadget)
                             offset = 0
+                            # counts number of return functions discovered
+                            nbret += 1
+                            # turns hex string extracted into disasCode to assembly instructions
                             disasCode = md.disasm_lite(gadget, offset)
                             strList = []
                             out = False
-                            isUseful = False
+                            isUseless = True
+                            # appends the assembly instructions into strList,
+                            # one entry for one assembly instruction
                             for (address, size, mnemonic, op_str) in disasCode:
                                 strList.append([address, mnemonic, op_str])
-                            # print str(strList[len(strList)-1][1])
-                            if strList and str(strList[-1][1]) == ('ret' or 'retq'):
-                                nbret += 1
+                            # checks that the list is not empty and that the last instruction is a ret
+                            if strList and str(strList[-1][1]) == ('ret' or 'retq' or 'retf' or 'retn'):
+                                # checks that the instructions in strList (taking only the required number, cfr length)
+                                # are not contained inside the list of bad instructions, such as jumps, calls, etc
+                                # furthermore I added a check to get only the instructions I want and find precise gadgets
                                 for a in strList[len(strList) - nbInstru - 1:len(strList) - 1]:
                                     if str(a[1]) in badInstruct:
                                         out = True
-                                    if str(a[1]) == 'pop' and str(a[2]) == 'rdi':
-                                        if str(a[1]) == 'syscall':  # and str(a[2]) == 'rsi':
-                                            isUseful = True
-                                if not out and isUseful:
+
+                                    # uncomment and modify the following two lines to enable specific gadget search
+                                    # if str(a[1]) == 'pop' and str(a[2]) == 'rsi':
+                                    #     isUseless = False
+                                # prints the selected gadgets along with their address offset
+                                if not out and isUseless:
                                     nbGadget += 1
-                                print 'gadget at %x : \n' % (i)
-                                for a in strList[len(strList) - nbInstru - 1:len(strList)]:
-                                    print ("%x      %s %s \n") % (a[0], a[1], a[2])
-                                # print ("%x      %s %s \n") % (strList[-1][0], strList[-1][1], strList[-1][2])
-                                # print '%s' % ' \n'.join(map(str, strList))
-                print nbGadget
-                print nbret
+                                    # print 'gadget at %x : \n' % (i- lengthHex + int(strList[0][0])+ int(strList[len(strList) - nbInstru - 1][0]))
+                                    for a in strList[len(strList) - nbInstru - 1:len(strList)]:
+                                        print ("%x      %s %s \n") % (a[0], a[1], a[2])
+                                    # print ("%x      %s %s \n") % (strList[-1][0], strList[-1][1], strList[-1][2])
+                                    # print '%s' % ' \n'.join(map(str, strList))
+                    print nbGadget
+                    print nbret
 
  # for filename in sys.argv[4:]:
             #     length = (int(sys.argv[3])*2)+2
