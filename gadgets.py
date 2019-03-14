21628b4d7c7307bab366316af68ef27b1a2701c1
Ny4nc4t <>
2019-03-14 13:19:08 +0100

    qw

--- gadgets.py
+++ gadgets.py
@@ -95,30 +95,25 @@ if __name__ == '__main__':
 
                             a = i-2
                             while a >= i-lengthHex and not out:
-                                print 2
                                 gadget = hexdata[a: i + 2]
-                                print gadget
                                 a -= 2
                                 gadget = convertXCS(gadget)
                                 offset = 0
                                 disasCode = md.disasm_lite(gadget, offset)
                                 strList = []
                                 for (address, size, mnemonic, op_str) in disasCode:
-                                    print 3
                                     if str(mnemonic) not in badInstruct :
-                                        print 4
                                         strList.append([address, mnemonic, op_str])
                                     else:
-                                        print 5
                                         out = True
 
                                 if strList and (str(strList[-1][1]) == ('ret' or 'retq' or 'retf' or 'retn')) and not out and len(strList) == nbInstru+1:
-                                    print 6
                                     for a in strList:
                                             print ("%x      %s %s \n") % (a[0], a[1], a[2])
                                     out = True
                                     nbGadget += 1
-
+                            print nbGadget
+                            print nbret
 
 
                             # takes the bytes before ret, depending on the length specified
@@ -165,8 +160,7 @@ if __name__ == '__main__':
                             #         # print '%s' % ' \n'.join(map(str, strList))
 
 
-                    print nbGadget
-                    print nbret
+
 
  # for filename in sys.argv[4:]:
             #     length = (int(sys.argv[3])*2)+2
