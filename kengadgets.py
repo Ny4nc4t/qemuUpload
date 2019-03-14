c830ef47096afeef7994837d8f36e11b09246d1e
Ny4nc4t <>
2019-03-14 16:13:43 +0100

    qw

--- kengadgets.py
+++ kengadgets.py
@@ -112,6 +112,7 @@ if __name__ == '__main__':
 
 
                             for ( mnemonic, op_str) in instList[- int(sys.argv[3])-1: -1]:
+                                print 'lol'
                                 if mnemonic in branInst:
                                     print ("mauvais  %s \n") % (mnemonic)
                                     flag = 0
