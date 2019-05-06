279f376459a04904130ef4cf44335b208d58dfa8
Ny4nc4t <>
2019-05-06 08:27:17 +0200

    qw

--- LookupMirror.java
+++ LookupMirror.java
@@ -2,7 +2,7 @@ public class Mirror{
         Class<?> lookupClass;
         int allowedModes;
 
-        public void setSecurityNull() {
-                System.setSecurityManager(null);
-        }
+//        public void setSecurityNull() {
+//                System.setSecurityManager(null);
+//        }
 }
