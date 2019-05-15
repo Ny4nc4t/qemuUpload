a352bc7ce0c0033bbef29cd05a89aadcffe88dd8
Ny4nc4t <>
2019-05-15 17:35:01 +0200

    qw

--- script.py
+++ script.py
@@ -5,3 +5,4 @@ request = "http://localhost/lab09/login.php?u=\" OR id = 98 AND SUBSTRING(passwo
 bashCommand = "echo 'hello' "
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
+print output
