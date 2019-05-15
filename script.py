8dd8546f0c22a2e1e765efafb9bc54641ac225bc
Ny4nc4t <>
2019-05-15 17:34:30 +0200

    qw

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import subprocess
 
 request = "http://localhost/lab09/login.php?u=\" OR id = 98 AND SUBSTRING(password,1,1) = '1' --   "
-%bashCommand = "./command.sh  " + request 
+#bashCommand = "./command.sh  " + request 
 bashCommand = "echo 'hello' "
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
