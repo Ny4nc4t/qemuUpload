03332b8c15a9109507bdcbef3bc5aae4cf6946e7
Ny4nc4t <>
2019-05-15 13:12:27 +0200

    qw

--- LookupMirror.java
+++
@@ -1,8 +0,0 @@
-public class LookupMirror{
-        Class<?> lookupClass;
-        int allowedModes;
-
-       public void setSecurityNull() {
-               System.setSecurityManager(null);
-       }
-}
